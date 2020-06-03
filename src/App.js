96154aa05eba046f85b1dea8bc7c03dc00ad66d3
Dhanmoni Nath <dhanmoninath989@gmail.com>
2020-06-03 19:49:08 +0530

    create a new context(themeContext) and provide it to our components with dark and light theme values

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 //import SongList from "./Hooks/Components/SongList";
 import Navbar from "./Context/Components/Navbar";
 import BookList from "./Context/Components/BookList";
+import ThemeContextProvider from "./Context/ContextAPI/ThemeContext";
 
 function App() {
   return (
     <div className="App">
       {/* <SongList /> */}
-      <Navbar />
-      <BookList />
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+      </ThemeContextProvider>
     </div>
   );
 }
