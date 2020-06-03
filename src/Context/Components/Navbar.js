a1d2a70aa2030079715cd1e9a67c617575f29568
Dhanmoni Nath <dhanmoninath989@gmail.com>
2020-06-03 21:33:06 +0530

    Navbar updated for using Consumer context and consuming multuple context added(Auth context & Theme context

--- src/Context/Components/Navbar.js
+++ src/Context/Components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { ThemeContext } from "../ContextAPI/ThemeContext";
+import { AuthContext } from "../ContextAPI/AuthContext";
 
 // const Navbar = () => {
 //   return (
@@ -15,20 +16,52 @@ import { ThemeContext } from "../ContextAPI/ThemeContext";
 // };
 
 //CONTEXT USING CLASS COMPONENT:
+// class Navbar extends Component {
+//   static contextType = ThemeContext;
+//   render() {
+// const { isLightTheme, light, dark } = this.context;
+// const theme = isLightTheme ? light : dark;
+//     return (
+//       <nav style={{ backgroundColor: theme.ui, color: theme.syntax }}>
+//         <h2>Context App</h2>
+//         <ul>
+//           <li>Home</li>
+//           <li>About</li>
+//           <li>Contact</li>
+//         </ul>
+//       </nav>
+//     );
+//   }
+// }
+
+//CONTEXT USING THEMECONTEXT.CONSUMER WITH CLASS COMPONENT:
 class Navbar extends Component {
-  static contextType = ThemeContext;
   render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
     return (
-      <nav style={{ backgroundColor: theme.ui, color: theme.syntax }}>
-        <h2>Context App</h2>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      <AuthContext.Consumer>
+        {(authContext) => (
+          <ThemeContext.Consumer>
+            {(themeContext) => {
+              const { isAuthenticated, toggleAuth } = authContext;
+              const { isLightTheme, light, dark } = themeContext;
+              const theme = isLightTheme ? light : dark;
+              return (
+                <nav style={{ backgroundColor: theme.ui, color: theme.syntax }}>
+                  <h2>Context App</h2>
+                  <div onClick={toggleAuth}>
+                    {isAuthenticated ? "Logged in" : "Logged out"}
+                  </div>
+                  <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                  </ul>
+                </nav>
+              );
+            }}
+          </ThemeContext.Consumer>
+        )}
+      </AuthContext.Consumer>
     );
   }
 }
