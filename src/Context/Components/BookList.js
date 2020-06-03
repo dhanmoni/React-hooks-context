ce3cad423f597bce0d6ad19418a7b2586a1fc224
Dhanmoni Nath <dhanmoninath989@gmail.com>
2020-06-03 20:53:47 +0530

    BookList component updated for using ThemeContext.consumer context instead of contextType, Navbar is still using contextType though

--- src/Context/Components/BookList.js
+++ src/Context/Components/BookList.js
@@ -1,36 +1,53 @@
 import React, { Component } from "react";
 import { ThemeContext } from "../ContextAPI/ThemeContext";
 
-// const BookList = () => {
-//   return (
-// <div className="book-list">
-//   <ul>
-//     <li>The way of kings</li>
-//     <li>The name of the wind</li>
-//     <li>The final empire</li>
-//   </ul>
-// </div>
-//   );
-// };
-
 //CONTEXT USING CLASS COMPONENT:
 
+// class BookList extends Component {
+//   static contextType = ThemeContext;
+//   render() {
+//     const { isLightTheme, light, dark } = this.context;
+//     const theme = isLightTheme ? light : dark;
+//     return (
+//       <div
+//         className="book-list"
+//         style={{ color: theme.syntax, backgroundColor: theme.bg }}
+//       >
+//         <ul>
+//           <li style={{ backgroundColor: theme.ui }}>The way of kings</li>
+//           <li style={{ backgroundColor: theme.ui }}>The name of the wind</li>
+//           <li style={{ backgroundColor: theme.ui }}>The final empire</li>
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+
+//CONTEXT USING THEMECONTEXT.CONSUMER WITH CLASS COMPONENT:
+
 class BookList extends Component {
-  static contextType = ThemeContext;
   render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
     return (
-      <div
-        className="book-list"
-        style={{ color: theme.syntax, backgroundColor: theme.bg }}
-      >
-        <ul>
-          <li style={{ backgroundColor: theme.ui }}>The way of kings</li>
-          <li style={{ backgroundColor: theme.ui }}>The name of the wind</li>
-          <li style={{ backgroundColor: theme.ui }}>The final empire</li>
-        </ul>
-      </div>
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <div
+              className="book-list"
+              style={{ color: theme.syntax, backgroundColor: theme.bg }}
+            >
+              <ul>
+                <li style={{ backgroundColor: theme.ui }}>The way of kings</li>
+                <li style={{ backgroundColor: theme.ui }}>
+                  The name of the wind
+                </li>
+                <li style={{ backgroundColor: theme.ui }}>The final empire</li>
+              </ul>
+            </div>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
