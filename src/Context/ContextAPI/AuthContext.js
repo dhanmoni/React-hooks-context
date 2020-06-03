41e87b9e0d2b17d45cc2a52264560a93cee01f47
Dhanmoni Nath <dhanmoninath989@gmail.com>
2020-06-03 22:16:54 +0530

    all contexts are updated to use functional component

--- src/Context/ContextAPI/AuthContext.js
+++ src/Context/ContextAPI/AuthContext.js
@@ -1,26 +1,37 @@
-import React, { Component, createContext } from "react";
+import React, { createContext, useState } from "react";
 
 export const AuthContext = createContext();
 
-class AuthContextProvider extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isAuthenticated: false,
-    };
-  }
-  toggleAuth = () => {
-    this.setState({ isAuthenticated: !this.state.isAuthenticated });
+const AuthContextProvider = (props) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const toggleAuth = () => {
+    setIsAuthenticated(!isAuthenticated);
   };
-  render() {
-    return (
-      <AuthContext.Provider
-        value={{ ...this.state, toggleAuth: this.toggleAuth }}
-      >
-        {this.props.children}
-      </AuthContext.Provider>
-    );
-  }
-}
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, toggleAuth }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+// class AuthContextProvider extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       isAuthenticated: false,
+//     };
+//   }
+//   toggleAuth = () => {
+//     this.setState({ isAuthenticated: !this.state.isAuthenticated });
+//   };
+//   render() {
+//     return (
+//       <AuthContext.Provider
+//         value={{ ...this.state, toggleAuth: this.toggleAuth }}
+//       >
+//         {this.props.children}
+//       </AuthContext.Provider>
+//     );
+//   }
+// }
 
 export default AuthContextProvider;
--- src/Context/ContextAPI/ThemeContext.js
+++ src/Context/ContextAPI/ThemeContext.js
@@ -1,37 +1,59 @@
-import React, { createContext, Component } from "react";
+import React, { createContext, Component, useState } from "react";
 
 export const ThemeContext = createContext();
 
-class ThemeContextProvider extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isLightTheme: true,
-      light: {
-        syntax: "#555",
-        ui: "#ddd",
-        bg: "#eee",
-      },
-      dark: {
-        syntax: "#ddd",
-        ui: "#333",
-        bg: "#555",
-      },
-    };
-  }
-
-  toggleTheme = () => {
-    this.setState({ isLightTheme: !this.state.isLightTheme });
+const ThemeContextProvider = (props) => {
+  const [isLightTheme, setIsLightTheme] = useState(true);
+  const [light] = useState({
+    syntax: "#555",
+    ui: "#ddd",
+    bg: "#eee",
+  });
+  const [dark] = useState({
+    syntax: "#ddd",
+    ui: "#333",
+    bg: "#555",
+  });
+  const toggleTheme = () => {
+    setIsLightTheme((isLightTheme) => !isLightTheme);
   };
-  render() {
-    return (
-      <ThemeContext.Provider
-        value={{ ...this.state, toggleTheme: this.toggleTheme }}
-      >
-        {this.props.children}
-      </ThemeContext.Provider>
-    );
-  }
-}
+  return (
+    <ThemeContext.Provider value={{ isLightTheme, light, dark, toggleTheme }}>
+      {props.children}
+    </ThemeContext.Provider>
+  );
+};
+
+// class ThemeContextProvider extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       isLightTheme: true,
+//       light: {
+// syntax: "#555",
+// ui: "#ddd",
+// bg: "#eee",
+//       },
+//       dark: {
+// syntax: "#ddd",
+// ui: "#333",
+// bg: "#555",
+//       },
+//     };
+//   }
+
+// toggleTheme = () => {
+//   this.setState({ isLightTheme: !this.state.isLightTheme });
+// };
+//   render() {
+//     return (
+//       <ThemeContext.Provider
+//         value={{ ...this.state, toggleTheme: this.toggleTheme }}
+//       >
+//         {this.props.children}
+//       </ThemeContext.Provider>
+//     );
+//   }
+// }
 
 export default ThemeContextProvider;
