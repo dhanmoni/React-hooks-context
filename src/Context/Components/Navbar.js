d74a2e05429face12bebd1eb935c3dc9b255f6ce
Dhanmoni Nath <dhanmoninath989@gmail.com>
2020-06-03 20:40:28 +0530

    theme context added to class based components

--- src/Context/Components/Navbar.js
+++ src/Context/Components/Navbar.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
+import { ThemeContext } from "../ContextAPI/ThemeContext";
 
-const Navbar = () => {
-  return (
-    <nav>
-      <h2>Context App</h2>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
-  );
-};
+// const Navbar = () => {
+//   return (
+// <nav>
+//   <h2>Context App</h2>
+//   <ul>
+//     <li>Home</li>
+//     <li>About</li>
+//     <li>Contact</li>
+//   </ul>
+// </nav>
+//   );
+// };
+
+//CONTEXT USING CLASS COMPONENT:
+class Navbar extends Component {
+  static contextType = ThemeContext;
+  render() {
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
+    return (
+      <nav style={{ backgroundColor: theme.ui, color: theme.syntax }}>
+        <h2>Context App</h2>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    );
+  }
+}
 
 export default Navbar;
